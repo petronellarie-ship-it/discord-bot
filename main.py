3675e93e5f4c078bd9676fd33244a62412323b1b
petronellarie-ship-it <petronellarie@gmail.com>
2026-06-21 19:03:28 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+import os
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} este online!")
+
+bot.run(TOKEN)
